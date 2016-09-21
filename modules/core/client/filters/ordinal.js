1da43372816cdd711850ace4da3c498681269703
Keith Levi Lumanog <keith@webninjamobile.com>
2016-09-21 16:53:18 +0800

    marriage

--- modules/core/client/filters/ordinal.js
+++ modules/core/client/filters/ordinal.js
@@ -1,9 +1,9 @@
 angular
   .module('core')
-  .filter('ordinal', function() {
-    return function(n) {
-      var s=["th","st","nd","rd"],
-        v=n%100;
-      return (s[(v-20)%10]||s[v]||s[0]);
+  .filter('ordinal', function () {
+    return function (n) {
+      var s = ['th', 'st', 'nd', 'rd'],
+        v = n % 100;
+      return (s[(v - 20) % 10] || s[v] || s[0]);
     };
-  })
+  });
